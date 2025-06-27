53d27f250823865874c90483c324a8739c7346d8
Phobez <36761666+Phobez@users.noreply.github.com>
2025-06-27 23:16:08 +0700

    Add composite names: Luonkuenese

--- generator.js
+++ generator.js
@@ -11,21 +11,48 @@ fetch('names.json')
 
 function generateNames(language, gender) {
   const nameList = document.getElementById("name-list");
-  
   nameList.innerHTML = "";
 
-  if (!names[language] || !names[language][gender]) {
+  const langData = names[language];
+  if (!langData) {
     nameList.innerHTML = "<li>No names available for this selection.</li>";
     return;
   }
 
-  const allNames = names[language][gender];
   const selectedNames = [];
 
-  while (selectedNames.length < 10 && selectedNames.length < allNames.length) {
-    const randomName = allNames[Math.floor(Math.random() * allNames.length)];
-    if (!selectedNames.includes(randomName)) {
-      selectedNames.push(randomName);
+  if (langData.type === "composite") {
+    const surnames = langData[surname];
+    const givenNames = langData[gender];
+
+    if (!surnames || !givenNames) {
+      nameList.innerHTML = `<li>No composite names available for ${language} ${gender}.</li>`;;
+      return;
+    }
+
+    while (selectedNames.length < 10 && selectedNames.length < surnames.length * givenNames.length) {
+      const surname = surnames[Math.floor(Math.random() * surnames.length)];
+      const givenName = givenNames[Math.floor(Math.random() * givenNames.length)];
+      const fullName = surname + " " + givenName;
+
+      if (!selectedNames.includes(fullName)) {
+        selectedNames.push(fullName);
+      }
+    }
+
+  } else if (langData.type === "simple") {
+    const allNames = langData[gender];
+
+    if (!allNames) {
+      nameList.innerHTML = `<li>No simple names available for ${language} ${gender}.</li>`;
+      return;
+    }
+
+    while (selectedNames.length < 10 && selectedNames.length < allNames.length) {
+      const randomName = allNames[Math.floor(Math.random() * allNames.length)];
+      if (!selectedNames.includes(randomName)) {
+        selectedNames.push(randomName);
+      }
     }
   }
 
@@ -35,3 +62,4 @@ function generateNames(language, gender) {
     nameList.appendChild(li);
   });
 }
+
